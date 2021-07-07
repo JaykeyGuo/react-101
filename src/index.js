2a458fbff2f063d8691deebf453d937c21f7562c
jaykeyguo <jaykeyguo@tencent.com>
2021-07-07 14:17:16 +0800

    add reverse list function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import judgeWinner from './judge.js'
+import judgeWinner from "./judge.js";
 
 function Square(props) {
   return (
-    <button
-      className="square"
-      onClick={props.onClick}
-    >
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -26,20 +23,76 @@ class Board extends React.Component {
   }
 
   render() {
-    const squares = [[0,1,2],[3,4,5],[6,7,8]];
+    const squares = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
     const squaresMap = squares.map((rows, index) => {
       return (
         <div className="board-row" key={index}>
           {rows.map((row, i) => {
-            return this.renderSquare(row)
+            return this.renderSquare(row);
           })}
         </div>
-      )
-    })
+      );
+    });
 
+    return <div>{squaresMap}</div>;
+  }
+}
+
+class Moves extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReverseMove: false,
+    };
+  }
+
+  jumpTo(step) {
+    this.props.handleJump(step);
+  }
+
+  reverseMove() {
+    this.setState({
+      isReverseMove: !this.state.isReverseMove,
+    });
+  }
+
+  renderMoves() {
+    const history = this.props.history;
+
+    let moves = history.map(({ position }, move) => {
+      const desc = move
+        ? `Go to move #${move}(${Math.floor(position / 3)}, ${position % 3})`
+        : "Go to game start";
+      return (
+        <li
+          key={move}
+          style={{ fontWeight: this.props.stepNumber === move ? 600 : 500 }}
+        >
+          <button
+            style={{ fontWeight: this.props.stepNumber === move ? 600 : 500 }}
+            onClick={() => this.jumpTo(move)}
+          >
+            {desc}
+          </button>
+        </li>
+      );
+    });
+    this.state.isReverseMove && (moves = moves.reverse());
+    return moves;
+  }
+
+  render() {
     return (
       <div>
-        {squaresMap}
+        <div>
+          <button onClick={() => this.reverseMove()}>Reverse</button>
+        </div>
+        {/* <div>{moves}</div> */}
+        <div>{this.renderMoves()}</div>
       </div>
     );
   }
@@ -49,13 +102,17 @@ class Game extends React.Component {
   constructor(props, ctx) {
     super(props);
     this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-        position: null,
-      }],
+      history: [
+        {
+          squares: Array(9).fill(null),
+          position: null,
+        },
+      ],
       stepNumber: 0,
       xIsNext: true,
-    }
+    };
+
+    // this.jumpTo = this.jumpTo.bind(this)
   }
 
   handleClick(i) {
@@ -65,53 +122,56 @@ class Game extends React.Component {
     if (squares[i] || judgeWinner(squares)) {
       return;
     }
-    squares[i] = this.state.xIsNext ? '❌' : '⭕️';
+    squares[i] = this.state.xIsNext ? "❌" : "⭕️";
     this.setState({
-      history: history.concat([{
-        squares,
-        position: i,
-      }]),
+      history: history.concat([
+        {
+          squares,
+          position: i,
+        },
+      ]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
-  jumpTo(step) {
+  // 使用箭头函数处理 this 的绑定问题
+  jumpTo = (step) => {
+    console.log(step, this);
     this.setState({
       // history: this.state.history.slice(0, step + 1),
       stepNumber: step,
-      xIsNext: (step % 2) === 0,
+      xIsNext: step % 2 === 0,
     });
-  }
-  
+  };
+
   render() {
     const history = this.state.history;
-    
-    let moves = history.map(({position}, move) => {
-      const desc = move
-      ? `Go to move #${move}(${Math.floor(position / 3)}, ${position % 3})`
-      : 'Go to game start';
-      return (
-        <li key={move} style={{ fontWeight: (this.state.stepNumber === move) ? 600 : 500 }}>
-          <button
-            style={{ fontWeight: (this.state.stepNumber === move) ? 600 : 500 }}
-            onClick={() => this.jumpTo(move)}>
-            {desc}
-          </button>
-        </li>
-      )
-    })
-    
+
+    // let moves = history.map(({position}, move) => {
+    //   const desc = move
+    //   ? `Go to move #${move}(${Math.floor(position / 3)}, ${position % 3})`
+    //   : 'Go to game start';
+    //   return (
+    //     <li key={move} style={{ fontWeight: (this.state.stepNumber === move) ? 600 : 500 }}>
+    //       <button
+    //         style={{ fontWeight: (this.state.stepNumber === move) ? 600 : 500 }}
+    //         onClick={() => this.jumpTo(move)}>
+    //         {desc}
+    //       </button>
+    //     </li>
+    //   )
+    // })
+
     const current = history[this.state.stepNumber];
     const winner = judgeWinner(current.squares);
     let status;
     if (winner) {
       status = `Winner: ${winner}`;
-    } else  {
-      status = `Next player: ${this.state.xIsNext ? '❌' : '⭕️'}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "❌" : "⭕️"}`;
     }
 
-    
     return (
       <div className="game">
         <div className="game-board">
@@ -122,7 +182,14 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{moves}</ol>
+          {/* <ol>{moves}</ol> */}
+          <ol>
+            <Moves
+              history={this.state.history}
+              stepNumber={this.state.stepNumber}
+              handleJump={this.jumpTo}
+            />
+          </ol>
         </div>
       </div>
     );
